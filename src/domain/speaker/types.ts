0bad253451cf646851a7a57a35a65ebdd3e425a9
sasaki-kodai <sasaki-kodai@kcgrp.jp>
2025-03-17 22:38:04 +0900

    add types for speaker definition

---
+++ src/domain/speaker/types.ts
@@ -0,0 +1,17 @@
+// Definition: Types for speaker
+
+export type Version = `${number}.${number}.${number}`;
+
+export type Speaker = {
+  name: string;
+  speaker_uuid: string;
+  styles: {
+    name: string;
+    id: number;
+    type: "talk";
+  }[];
+  version: Version;
+  supported_features: {
+    permitted_synthesis_morphing: "ALL" | "SELF_ONLY" | "NOTHING";
+  };
+};
